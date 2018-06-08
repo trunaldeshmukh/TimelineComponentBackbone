c0c940deb33f2c0ce83851e0a80b67feb1bd3bdf
Trunal Deshmukh <tdeshmukh@tavisca.com>
2018-06-08 09:25:07 +0530

    Template rendering at body end issue resolved

--- app.js
+++ app.js
@@ -2,17 +2,17 @@ var app = app || {};
 app.Models = app.Models || {};
 app.Views = app.Views || {};
 app.Collections = app.Collections || {};
-app.Variables=app.Variables||{};
+app.Variables = app.Variables || {};
 
 $(function () {
   //  audit_model=new app.Models.AuditModel({});
-   // audit_view=new app.Views.AuditModelView({model:audit_model});
-    //audit_view.render();
+  // audit_view=new app.Views.AuditModelView({model:audit_model});
+  //audit_view.render();
 
 
-    audit_collection_view = new app.Views.AuditCollectionView({ collection: app.Collections.audit_collection });
-     $(document.body).append(audit_collection_view.render().el);
+  audit_collection_view = new app.Views.AuditCollectionView({ collection: app.Collections.audit_collection });
+  audit_collection_view.render();
 
-     audit_collection_fullview = new app.Views.AuditCollectionFullView({ collection: app.Collections.audit_collection });
-     $(document.body).append(audit_collection_fullview.render().el);
+  audit_collection_fullview = new app.Views.AuditCollectionFullView({ collection: app.Collections.audit_collection });
+  audit_collection_fullview.render();
 });

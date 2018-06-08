41303902511ef2d7258f7771ee3e8e82db8aba68
Trunal Deshmukh <tdeshmukh@tavisca.com>
2018-06-08 16:59:41 +0530

    removed modal

--- Collections/AuditCollection.js
+++ Collections/AuditCollection.js
@@ -7,9 +7,9 @@ $(function () {
     });
 
     app.Collections.audit_collection = new app.Collections.AuditCollection();
-    app.Collections.audit_collection.add(new app.Models.AuditModel({ audit_id: 1, audit_event: "service enabled", audit_date: new Date().toDateString(), audit_comment: "1: audit added" }));
-    app.Collections.audit_collection.add(new app.Models.AuditModel({ audit_id: 2, audit_event: "service disabled", audit_date: new Date().toDateString(), audit_comment: "2: audit removed", timeline_dot_color:"#4caf50",timeline_icon_class:"icon md-image" }));
-    app.Collections.audit_collection.add(new app.Models.AuditModel({ audit_id: 3, audit_event: "service modified", audit_date: new Date().toDateString(), audit_comment: "3: audit modified" , timeline_dot_color:"#ff9800",timeline_icon_class:"icon md-file"}));
+    app.Collections.audit_collection.add(new app.Models.AuditModel({ audit_id: 1, audit_event: "Service Enabled", audit_date: new Date().toDateString(), audit_comment: "1: audit added" }));
+    app.Collections.audit_collection.add(new app.Models.AuditModel({ audit_id: 2, audit_event: "Service Disabled", audit_date: new Date().toDateString(), audit_comment: "2: audit removed", timeline_dot_color:"#4caf50",timeline_icon_class:"icon md-image" }));
+    app.Collections.audit_collection.add(new app.Models.AuditModel({ audit_id: 3, audit_event: "Service modified", audit_date: new Date().toDateString(), audit_comment: "3: audit modified" , timeline_dot_color:"#ff9800",timeline_icon_class:"icon md-file"}));
 
     app.Collections.audit_collection.add(new app.Models.AuditModel({ audit_id: 1, audit_event: "service enabled", audit_date: new Date("2018-05-01").toDateString(), audit_comment: "1: audit added" }));
     app.Collections.audit_collection.add(new app.Models.AuditModel({ audit_id: 2, audit_event: "service disabled", audit_date: new Date("2018-05-01").toDateString(), audit_comment: "2: audit removed", timeline_dot_color:"#4caf50",timeline_icon_class:"icon md-image" }));
